2ea5a653dd81b2a03ddf337cf693b6fa66cc24df
Joachim Meyer <jmeyer@boisestate.edu>
2025-06-04 17:05:30 -0600

    Refactor - Remove points_to_* methods from conversions
    Those were copied over when splitting the repository. Can always bring
    them back by importing from snowexsql, where they currently live too.

--- snowex_db/conversions.py
+++ snowex_db/conversions.py
@@ -5,17 +5,11 @@ of the database.
 """
 from os.path import basename, dirname, join
 
-import geopandas as gpd
-import pandas as pd
 import numpy as np
 import rasterio
-from geoalchemy2.shape import to_shape
-from rasterio import MemoryFile
 from rasterio.crs import CRS
 from rasterio.transform import Affine
-from sqlalchemy.dialects import postgresql
 
-from snowexsql.data import PointData
 from .utilities import get_logger
 
 
@@ -115,90 +109,3 @@ def INSAR_to_rasterio(grd_file, desc, out_file):
             # for stat in ['min','max','mean','std']:
             #     log.info('{} {} = {}'.format(comp, stat, getattr(d, stat)()))
             dataset.close()
-
-
-def points_to_geopandas(results):
-    """
-    Converts a successful query list into a geopandas data frame
-
-    Args:
-        results: List of PointData objects
-
-    Returns:
-        df: geopandas.GeoDataFrame instance
-    """
-    # grab all the attributes of the class to assign
-    if isinstance(results[0], PointData):
-        data = {a: [] for a in dir(PointData) if a[0:1] != '__'}
-
-    for r in results:
-        for k in data.keys():
-            v = getattr(r, k)
-
-            if k == 'geom':
-                v = to_shape(v)
-            data[k].append(v)
-
-    df = gpd.GeoDataFrame(data, geometry=data['geom'])
-    return df
-
-
-def query_to_geopandas(query, engine, **kwargs):
-    """
-    Convert a GeoAlchemy2 Query meant for postgis to a geopandas dataframe. Requires that a geometry column is
-    included
-
-    Args:
-        query: GeoAlchemy2.Query Object
-        engine: sqlalchemy engine
-
-    Returns:
-        df: geopandas.GeoDataFrame instance
-    """
-    # Fill out the variables in the query
-    sql = query.statement.compile(dialect=postgresql.dialect())
-
-    # Get dataframe from geopandas using the query and engine
-    df = gpd.GeoDataFrame.from_postgis(sql, engine, **kwargs)
-
-    return df
-
-def query_to_pandas(query, engine, **kwargs):
-    """
-    Convert a GeoAlchemy2 Query meant for postgis to a pandas dataframe.
-
-    Args:
-        query: Query Object
-        engine: sqlalchemy engine
-
-    Returns:
-        df: pandas.DataFrame instance
-    """
-    # Fill out the variables in the query
-    sql = query.statement.compile(dialect=postgresql.dialect())
-
-    # Get dataframe from geopandas using the query and engine
-    df = pd.read_sql(sql, engine, **kwargs)
-
-    return df
-
-def raster_to_rasterio(session, rasters):
-    """
-    Retrieve the numpy array of a raster by converting to a temporary file
-
-    Args:
-        session: sqlalchemy session object
-        raster: list of :py:class:`geoalchemy2.types.Raster`
-
-    Returns:
-        dataset: list of rasterio datasets
-
-    """
-    datasets = []
-    for r in rasters:
-        bdata = bytes(r[0])
-
-        with MemoryFile() as tmpfile:
-            tmpfile.write(bdata)
-            datasets.append(tmpfile.open())
-    return datasets
