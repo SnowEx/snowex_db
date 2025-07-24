cb7cd938571aabf31141e1c7fdd811f194bd1397
Joachim Meyer <jmeyer@boisestate.edu>
2025-07-24 09:48:19 -0600

    ProfileData - Remove overwrite of super init method.
    Call to super did not change anything.

--- snowex_db/profile_data.py
+++ snowex_db/profile_data.py
@@ -1,25 +1,10 @@
-from pathlib import Path
-
-import pandas as pd
-from insitupy.campaigns.snowex import (
-    SnowExProfileData, SnowExProfileDataCollection
-)
-from insitupy.io.metadata import MetaDataParser
-from insitupy.profiles.metadata import ProfileMetaData
-from insitupy.variables import MeasurementDescription
+from insitupy.campaigns.snowex import SnowExProfileData, SnowExProfileDataCollection
 
 from .metadata import ExtendedSnowExMetadataParser
 
 
 class ExtendedSnowexProfileData(SnowExProfileData):
     META_PARSER = ExtendedSnowExMetadataParser
-
-    def __init__(
-        self,
-        variable: MeasurementDescription,
-        meta_parser: MetaDataParser
-    ):
-        super().__init__(variable, meta_parser)
 
 
 class ExtendedSnowExProfileDataCollection(SnowExProfileDataCollection):
