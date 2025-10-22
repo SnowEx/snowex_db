7e66a2928fc7f77b518d6a942228a0a23841275d
Micah Sandusky <micc5725@gmail.com>
2025-10-22 15:11:52 -0600

    Working more on AK lidar

--- scripts/upload/add_AK_lidar.py
+++ scripts/upload/add_AK_lidar.py
@@ -26,12 +26,13 @@ LIDAR_DOI = {
 }
 
 
-def main():
+def main(files, doi):
     """
     Uploader script partial SnowEx Lidar
     """
     EPSG = 26906  # EPSG code for Alaska Zone 6
 
+    # TODO: expand to other sites as they become available
     # Typical kwargs for the dataset
     kwargs = {
         'instrument': 'lidar',
@@ -40,7 +41,7 @@ def main():
         'tiled': True,
         'no_data': -9999,
         'timezone': 'AKST',
-        'doi': 'https://doi.org/10.5067/BV4D8RRU1H7U',
+        'doi': doi,
         # TODO: what is the campaign name?
         "campaign_name": "Alaska 2023"
         }
@@ -48,20 +49,21 @@ def main():
     # Directory of SNOWEX products
     lidar_dir = Path('../download/data/SNOWEX/SNEX23_Lidar.001/')
     reprojected = lidar_dir.joinpath('reprojected')
+    reprojected.mkdir(exist_ok=True, parents=True)
 
     if not reprojected.is_dir():
         os.mkdir(reprojected)
 
     # Reproject using GDAL
     print('Reprojecting files...')
-    raw_files = lidar_dir.glob('*/*.tif')
-    for f in raw_files:
+    for f in files:
         # Watch out for files already in the reprojection
+        f = Path(f)
         if f.parent != reprojected:
             output = reprojected.joinpath(f.name)
-            cmd = f'gdalwarp -overwrite -t_srs EPSG:{kwargs["epsg"]} {f} {output}'
+            cmd = f'gdalwarp -overwrite -t_srs EPSG:{EPSG} {f} {output}'
             print(cmd)
-            p = Popen(cmd,shell=True)
+            p = Popen(cmd, shell=True)
             p.wait()
 
     ######################################## Farmers/Creamers Field (FLCF) ###################################################
@@ -69,14 +71,14 @@ def main():
     with db_session_with_credentials() as (_engine, session):
         # SWE flights
         for f, dt, raster_type in [
-            ("SNEX23_Lidar_FLCF_CH_0.25M_20221024_V01.0.tif",
+            ("SNEX23_Lidar_FLCF_CH_0.5M_20221024_V01.0.tif",
              date(2022, 10, 24), RasterType.CANOPY_HEIGHT),
-            ("SNEX23_Lidar_FLCF_SD_0.25M_20230311_V01.0.tif",
+            ("SNEX23_Lidar_FLCF_SD_0.5M_20230311_V01.0.tif",
              date(2023, 3, 11), RasterType.DEPTH),
         ]:
             fpath = reprojected.joinpath(f)
             raster_metadata = metadata_from_single_file(
-                Path(fpath), date=dt, type=RasterType.SWE,
+                Path(fpath), RasterType.SWE, date=dt,
                 **kwargs
             )
             u = UploadRaster(
@@ -86,6 +88,11 @@ def main():
 
 # Add this so you can run your script directly without running run.py
 if __name__ == '__main__':
-    for data_set_id, doi in SMP_DOI.items():
-        with get_files(data_set_id, doi) as files:
+    for data_set_id, doi in LIDAR_DOI.items():
+        with get_files(
+                data_set_id, doi, key_words=[
+                    "SNEX23_Lidar_FLCF_CH_0.5M_20221024_V01.0.tif",
+                    "SNEX23_Lidar_FLCF_SD_0.5M_20230311_V01.0.tif"
+                ]
+        ) as files:
             main(files, doi)
