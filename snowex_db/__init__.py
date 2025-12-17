62500c5a12ea4ca02433764ff15a41a5961696eb
aaarendt <arendta@uw.edu>
2025-12-17 08:15:43 -0800

    fix: delete default database connection in init

--- snowex_db/__init__.py
+++ snowex_db/__init__.py
@@ -3,15 +3,15 @@
 __author__ = """Micah Johnson"""
 __version__ = '0.1.0'
 
-from snowexsql.db import get_db
-from snowexsql.api import DB_NAME
-from contextlib import contextmanager
+# from snowexsql.db import get_db
+# from snowexsql.api import DB_NAME
+# from contextlib import contextmanager
 
 
-@contextmanager
-def db_session(db_name, credentials):
-    # use default_name
-    db_name = db_name or DB_NAME
-    engine, session = get_db(db_name, credentials=credentials)
-    yield session, engine
-    session.close()
+# @contextmanager
+# def db_session(db_name, credentials):
+#     # use default_name
+#     db_name = db_name or DB_NAME
+#     engine, session = get_db(db_name, credentials=credentials)
+#     yield session, engine
+#     session.close()
